2561b97b8b948fe189e17a270f09a35b21e0118b
Adam Jordan <jordana@sea.com>
2022-12-01 15:42:51 +0700

    Add add-user.sh

---
+++ add-user.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+user="adam"
+key_url="https://github.com/adamyordan.keys"
+
+# add user
+adduser --disabled-password --gecos "" "${user}"
+
+# add user as sudoer
+usermod -aG sudo "${user}"
+echo "${user} ALL=(root) NOPASSWD: ALL" | tee /etc/sudoers.d/sec
+
+# add public key
+mkdir /home/"${user}"/.ssh
+wget ${key_url} -O /home/"${user}"/.ssh/authorized_keys
+chmod 700 /home/"${user}"/.ssh
+chmod 600 /home/"${user}"/.ssh/authorized_keys
+chown -R "${user}":"${user}" /home/"${user}"/.ssh
